19417e89233f09f8e2ed6346490c69cb45bcfb53
braemy <raemybaptiste@gmail.com>
2016-12-19 10:10:48 +0100

    tried to implement static and dynamic word embedding using GloVe

--- TextCNN.py
+++ TextCNN.py
@@ -24,43 +24,51 @@ class TextCNN(object):
         self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
 
         #First layer: Embedding layer
-        with tf.device('/cpu:0'), tf.name_scope("embedding"):
-            W = tf.Variable(
+        with tf.device('/cpu:0'), tf.name_scope("embedding_static"):
+            self.W_static = tf.Variable(
                 tf.random_uniform([vocab_size, embedding_dim], -1.0, 1.0),
-                name="W")
-            self.embedded_chars = tf.nn.embedding_lookup(W, self.input_x)
-            self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
+                name="W", trainable=False)
+            self.embedded_chars_static = tf.nn.embedding_lookup(self.W_static, self.input_x)
+            self.embedded_chars_expanded_static = tf.expand_dims(self.embedded_chars_static, -1)
 
-
+        with tf.device('/cpu:0'), tf.name_scope("embedding_dynamic"):
+            self.W_dynamic = tf.Variable(
+                tf.random_uniform([vocab_size, embedding_dim], -1.0, 1.0),
+                name="W", trainable=True)
+            self.embedded_chars_dynamic = tf.nn.embedding_lookup(self.W_dynamic, self.input_x)
+            self.embedded_chars_expanded_dynamic = tf.expand_dims(self.embedded_chars_dynamic, -1)
 
 
         # Convolution and max pooling layers
         pooled_outputs = []
+        embedded_chars_expandeds = [self.embedded_chars_expanded_static, self.embedded_chars_expanded_dynamic]
+
         for i, filter_size in enumerate(filter_sizes):
-            with tf.name_scope("conv-maxpool-%s" % filter_size):
-                # Convolution Layer
-                filter_shape = [filter_size, embedding_dim, 1, num_filters]
-                W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
-                b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
-                conv = tf.nn.conv2d(
-                    self.embedded_chars_expanded,
-                    W,
-                    strides=[1, 1, 1, 1],
-                    padding="VALID",
-                    name="conv")
-                # Apply nonlinearity
-                h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
-                # Max-pooling over the outputs
-                pooled = tf.nn.max_pool(
-                    h,
-                    ksize=[1, sequence_length - filter_size + 1, 1, 1],
-                    strides=[1, 1, 1, 1],
-                    padding='VALID',
-                    name="pool")
-                pooled_outputs.append(pooled)
+            for j,emb in enumerate(embedded_chars_expandeds):
+                with tf.name_scope("conv-maxpool-{}-{}".format(filter_size, j)):
+                    # Convolution Layer
+                    filter_shape = [filter_size, embedding_dim, 1, num_filters]
+                    W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
+                    b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
+                    conv = tf.nn.conv2d(
+                        emb,
+                        W,
+                        strides=[1, 1, 1, 1],
+                        padding="VALID",
+                        name="conv")
+                    # Apply nonlinearity
+                    h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
+                    # Max-pooling over the outputs
+                    pooled = tf.nn.max_pool(
+                        h,
+                        ksize=[1, sequence_length - filter_size + 1, 1, 1],
+                        strides=[1, 1, 1, 1],
+                        padding='VALID',
+                        name="pool")
+                    pooled_outputs.append(pooled)
 
         # Combine all the pooled features
-        num_filters_total = num_filters * len(filter_sizes)
+        num_filters_total = num_filters * len(filter_sizes) * 2 #TODO *2 is because of the 2 channels. Not sure
         self.h_pool = tf.concat(3, pooled_outputs)
         self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total])
 
