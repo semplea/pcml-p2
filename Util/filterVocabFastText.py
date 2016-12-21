26ee04c3c8f83f3fb156b1ac4918a685fe3d3adf
Loic Ottet <loic.ottet@epfl.ch>
2016-12-21 15:21:21 +0100

    Bugs in filterVocabFastText

--- Util/filterVocabFastText.py
+++ Util/filterVocabFastText.py
@@ -2,7 +2,7 @@ import numpy as np
 import pickle
 import sys
 
-def filterVocab(file, vocab, size):
+def filterVocab(file, vocab, size, full=False):
 	with open(file) as f:
 		with open(vocab, "rb") as voc:
 			v = pickle.load(voc)
@@ -26,9 +26,9 @@ def filterVocab(file, vocab, size):
 			print(len(found))
 			print(embeddings.shape)
 
-	np.save("embeddings_fasttext_full", embeddings)
+	np.save("embeddings_fasttext" + ("_full" if full else ""), embeddings)
 	#pickle.dump(v, open("vocab_glove.pkl", "wb"))
 
 if __name__ == '__main__':
 	full = len(sys.argv) >= 2 and sys.argv[2] == "full"
-	filterVocab("twitter-datasets/model.vec", "twitter-datasets/" + ("vocab_full" if full else "vocab") + ".pkl", dim)
+	filterVocab("twitter-datasets/model.vec", "twitter-datasets/" + ("vocab_full" if full else "vocab") + ".pkl", 100, full)
