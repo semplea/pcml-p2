16e41f7ec146c6a6a493e72a9d36e3fbeb183ba8
Loic Ottet <loic.ottet@epfl.ch>
2016-12-21 13:58:48 +0100

    Final checkpoint

--- trainTensorflow.py
+++ trainTensorflow.py
@@ -23,13 +23,13 @@ tf.flags.DEFINE_float("dev_sample_percentage", 0.05, "Percentage of the training
 tf.flags.DEFINE_string("filter_sizes", "3,4,5", "Comma-separated filter sizes (default: '3,4,5')")
 tf.flags.DEFINE_integer("num_filters", 128, "Number of filters per filter size (default: 128)")
 tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
-tf.flags.DEFINE_float("l2_reg_lambda", 0.1, "L2 regularizaion lambda (default: 0.0)")
+tf.flags.DEFINE_float("l2_reg_lambda", 1e-5, "L2 regularizaion lambda (default: 0.0)")
 
 # Training parameters
 tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
-tf.flags.DEFINE_integer("num_epochs", 2, "Number of training epochs (default: 200)")
+tf.flags.DEFINE_integer("num_epochs", 2, "Number of training epochs (default: 2)")
 tf.flags.DEFINE_integer("evaluate_every", 100, "Evaluate model on dev set after this many steps (default: 100)")
-tf.flags.DEFINE_integer("checkpoint_every", 100, "Save model after this many steps (default: 100)")
+tf.flags.DEFINE_integer("checkpoint_every", 2000, "Save model after this many steps (default: 100)")
 
 # Misc parameters
 tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
@@ -225,3 +225,7 @@ with tf.Graph().as_default():
 			if current_step % FLAGS.checkpoint_every == 0:
 				path = saver.save(sess, checkpoint_prefix, global_step=current_step)
 				print("Saved model checkpoint to {}\n".format(path))
+
+		# Final checkpoint
+		path = saver.save(sess, checkpoint_prefix, global_step=current_step)
+		print("Saved model checkpoint to {}\n".format(path))
